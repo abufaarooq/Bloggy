195f9f3c9c2b1eaa4292371c463048ebe00f6920
Abdirahman Abdirahman <Abufaarooq@icloud.com>
2023-07-11 11:57:21 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const articleRouter = require('./routes/articles')
+
+const app = express()
+
+app.set('view engine','ejs')
+
+app.use('/articles', articleRouter)
+
+
+app.get('/', (req, res) => { 
+    const article = [{
+        title: 'Test Article',
+        timeCreated: new Date(),
+        description: 'Test Description'
+    }]
+    res.render("articles/index.ejs", {articles: article})
+})
+
+app.listen(5000)
